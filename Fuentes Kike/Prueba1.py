8c623b40cf92c2c11d441634fb8d616e256eba6e
Roca Segi, Enric <enric.roca@allianz.es>
2020-06-15 15:37:59 +0200

    HolaMundos

---
+++ Fuentes Kike/Prueba1.py
@@ -0,0 +1,10 @@
+print ("Hello World!")
+print ("I'm Kike, and this is my first Python program")
+
+print (2+2)
+n = 2+2
+
+if n > 4:
+    print ('2+2 es mayor que 4')
+else:
+    print ('2+2 no es mayor que 4')
