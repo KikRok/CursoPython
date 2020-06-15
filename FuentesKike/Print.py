571434efb6df712f99f97f5359868ce91fe8b8c7
Roca Segi, Enric <enric.roca@allianz.es>
2020-06-15 15:52:26 +0200

    Ejercicions de print

---
+++ FuentesKike/Print.py
@@ -0,0 +1,7 @@
+x =42
+print ('Hello, World.{}'.format(x))
+
+s = 'Hello, World.{}'.format(x)  # {} para temas de formatos 
+print (s)
+
+print ('Hello %d' %x)   #para apuntar a variables y hacerles referencia.
